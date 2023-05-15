e0d70d359047ff3e0e263658c94cad87d1b18e95
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2023-05-15 16:09:08 -0300

    Adicionei metodo criar dados codinome

--- UOL/src/main/java/com/cadastroJogador/UOL/codinome/CodinomeController.java
+++ UOL/src/main/java/com/cadastroJogador/UOL/codinome/CodinomeController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -21,6 +25,12 @@ public class CodinomeController {
 	@GetMapping
 	public List<Codinome> listar() {
 		return repository.findAll();
+	}
+	
+	@PostMapping
+	@Transactional
+	public void criar(@RequestBody DadosCadastroCodinome dados) {
+		repository.save(new Codinome(dados));
 	}
 	
 	@DeleteMapping("{id}")
